3cbe545016e45fa3178fe4f6d86eb8dd395c79b6
Harshendra Shah <harshendrashah812@gmail.com>
2017-11-03 14:20:51 +0530

    Added Lab1

---
+++ Lab 1/Ramp.m
@@ -0,0 +1,13 @@
+n=1:20;
+t=1:100;
+x=t;
+subplot(2,1,1);
+plot(t,x,'r');
+title("Ramp function in continuous time");
+xlabel('Time');
+ylabel('Y');
+subplot(2,1,2);
+stem(t,x,'g');
+title("Ramp function in discrete time");
+xlabel('Time');
+ylabel('Y');
---
+++ Lab 1/UnitIstep.m
@@ -0,0 +1,13 @@
+n=100;
+t=1:100;
+x=ones(1,100);
+subplot(2,1,1);
+plot(t,x,'r');
+title("Unit Step function in continuous time");
+xlabel('Time');
+ylabel('Amplitude');
+subplot(2,1,2);
+stem(t,x,'g');
+title("Unit Step function in discrete time");
+xlabel('Index');
+ylabel('Amplitude');
