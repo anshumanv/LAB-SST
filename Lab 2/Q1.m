f14489fb585d5db12d9f427fcf4a9af9515b5adc
MAYANK PATHELA <mp.pathela@gmail.com>
2017-11-16 09:22:46 +0530

    Changed files of Lab2 (#10)
    * Added Lab7 Codes
    
    * Update and rename Q1.m to Q2.m
    
    * Rename q2.m to Q1.m

--- Lab 2/Q1.m
+++ Lab 2/Q1.m
@@ -1,28 +1,88 @@
-n=1:5;
-x1=[1,2,3,4,5];
-a=(mod(x1,2)==0);
-b=(mod(x1,2)!=0);
+t=0:0.01:2;
+x1=6*sin(2*pi*5*t);
+x2=8*sin(2*pi*5*t);
 
-subplot(3,1,1);
-plot(n,a,'b');
-title("Even parts of the sequence");
-xlabel("Index");
-ylabel("Value");
+subplot(4,3,1);
+plot(t,x1,'b');
+title("Plot of x1");
+xlabel("Time");
+ylabel("x1");
 
-subplot(3,1,2);
-plot(n,b,'g');
-title("Odd parts of the sequence");
-xlabel("Index");
-ylabel("Value");
+subplot(4,3,2);
+plot(t,x2,'g');
+title("Plot of x2");
+xlabel("Time");
+ylabel("x2");
+x3=x1+x2;
 
-x2=[complex(1,6),complex(2,5),3,complex(4,3),complex(0,5)];
-a=[];
-b=[];
-a=real(x2);
-b=imag(x2);
+subplot(4,3,3);
+plot(t,x3,'r');
+title("Addition");
+xlabel("Time");
+ylabel("X");
+x4=x1.*x2;
 
-subplot(3,1,3);
-plot(a,b,'r');
-title("Real and imagenary parts of the sequence");
-xlabel("Real part");
-ylabel("Imagenary part");
+subplot(4,3,4);
+plot(t,x4,'y');
+title("Multiplication");
+xlabel("Time");
+ylabel("X");
+x5=6*x1;
+
+subplot(4,3,5);
+plot(t,x5,'b');
+title("Amplitude scaling of x1");
+xlabel("Time");
+ylabel("x1");
+x5=6*x2;
+
+subplot(4,3,6);
+plot(t,x5,'g');
+title("Amplitude scaling of x2");
+xlabel("Time");
+ylabel("x2");
+t1=2*t;
+x5=6*sin(2*pi*5*t1);
+
+subplot(4,3,7);
+plot(t,x5,'b');
+title("Time scaling of x1");
+xlabel("Time");
+ylabel("x1");
+x5=8*sin(2*pi*5*t1);
+
+subplot(4,3,8);
+plot(t,x5,'g');
+title("Time scaling of x1");
+xlabel("Time");
+ylabel("x2");
+t1 = t*1.5;
+x5=6*sin(2*pi*5*t1);
+
+subplot(4,3,9);
+plot(t,x5,'b');
+title("Time shifting of x1");
+xlabel("Time");
+ylabel("x1");
+x5=8*sin(2*pi*5*t1);
+
+subplot(4,3,10);
+plot(t,x5,'g');
+title("Time shifting of x2");
+xlabel("Time");
+ylabel("x2");
+t1 = t;
+x5=6*sin(2*pi*5*t1);
+
+subplot(4,3,11);
+plot(t,x5,'b');
+title("Time reversal of x1");
+xlabel("Time");
+ylabel("x1");
+
+x5=8*sin(2*pi*5*t1);
+subplot(4,3,12);
+plot(t,x5,'g');
+title("Time reversal of x2");
+xlabel("Time");
+ylabel("x2");
