ba15740d408cc71213012b1bdee6bae680d92b6b
Anshuman Verma <anshu.av97@gmail.com>
2017-11-16 11:22:16 +0530

    Update Ans2.m

--- Lab 8/Ans2.m
+++ Lab 8/Ans2.m
@@ -1,17 +1,18 @@
+
 pkg load image;
 
-im=imread("cameraman.tif"); 	%image file as input
-mask = [1 1 1 1 ; 1 1 1 1 ; 1 1 1 1 ; 1 1 1 1]/16;
-p_size=paddedsize(size(im));
-mask=fft2(double(mask), p_size(1), p_size(2));
-F=fft2(double(im),p_size(1),p_size(2));
+im=imread('cameraman.tif');
+mask=[1 1 1 1;1 1 1 1;1 1 1 1;1 1 1 1]/16;
+
+mask=fft2(double(mask),size(im,1)*2,size(im,2)*2);
+F=fft2(double(im),size(im,1)*2,size(im,2)*2);
+
 FDF=mask.*F;
 fdf=ifft2(FDF);
+figure(1);
+imshow(fdf,[]);
 fdf=fdf(1:size(im,1),1:size(im,2));
-         
-figure(1)
+figure(2);
 imshow(im,[]);
-
-        
-figure(2)
+figure(3);
 imshow(fdf,[]);
